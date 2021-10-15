0a39a188541b52bbc4ab22e382f8132fdf95bc8a
MatiasMarro <matiasmarro1987@gmail.com>
2021-10-15 15:30:07 -0300

    Create objects: 'PELOTA' and 'JUGADOR'. Movement devoloped

--- main.cpp
+++ main.cpp
@@ -1,18 +1,62 @@
 #include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
 #include "libgame.h"
+
+
+class JUGADOR {
+    int x,y;
+public:
+    JUGADOR(int _x, int _y);
+    void pintar() const ;
+    void borrar() const;
+    void Y(int _y) {y += _y;}
+};
+
+JUGADOR::JUGADOR(int _x, int _y): x(_x), y(_y){}
+
+void JUGADOR::pintar() const{
+
+    gotoxy(x,y-1); printf("%c",219);
+    gotoxy(x,y);   printf("%c",219);
+    gotoxy(x,y+1); printf("%c",219);
+
+}
+void JUGADOR::borrar() const{
+    gotoxy(x,y-1); printf(" ");
+    gotoxy(x,y);   printf(" ");
+    gotoxy(x,y+1); printf(" ");
+}
+
 
 int main(){
     OcultaCursor();
-    Color(ROJO);
-    inicia_libgame();
-    Color(AMARILLO);
-    gotoxy(22,8); printf("Modificado por Matias Marro");
-    Color(AZUL);
-    gotoxy(26,15); printf("https://github.com/MatiasMarro");
-    Color(VERDE2);
-    gotoxy(22,13); printf("PONER REPOSITORIO AQUI");
-    Color(MORADO);
     pintar_marco();
-    //pausa();
-    pausa(900);
+
+    JUGADOR A(6,15);  A.pintar();
+    JUGADOR B(74,15); B.pintar();
+
+    char tecla;
+
+    while(1){
+        A.borrar();     B.borrar();
+
+        if(kbhit()){
+            A.borrar(); B.borrar();
+            tecla = getch();
+            if(tecla == 'q') A.Y(-1); else if(tecla == 'a') A.Y(1);
+            if(tecla == 'o') B.Y(-1); else if(tecla == 'l') B.Y(1);
+            A.pintar(); B.pintar();
+        }
+
+        A.pintar(); B.pintar();
+
+
+        pausa(10);
+
+    }
+
+
+    return 0;
 }
